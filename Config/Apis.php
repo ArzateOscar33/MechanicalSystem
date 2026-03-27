c6bd82f931b685aef9159da1d4d39a5d41744a70
ArzateOscar33 <oscar.arzate201@tectijuana.edu.mx>
2026-03-27 15:02:03 -0700

    Update Apis.php

--- Config/Apis.php
+++ Config/Apis.php
@@ -2,8 +2,8 @@
 // Config/Apis.php
 
 // ===== Secomext =====
-defined('SECOMEXT_USER')         || define('SECOMEXT_USER',        'mechemission2');
-defined('SECOMEXT_PASS')         || define('SECOMEXT_PASS',        'AFf0qvB38ffa$');
+defined('SECOMEXT_USER')         || define('SECOMEXT_USER', 'mechemission2');
+defined('SECOMEXT_PASS')         || define('SECOMEXT_PASS', 'AFf0qvB38ffa$');
 defined('SECOMEXT_WORKSHOP_ID')  || define('SECOMEXT_WORKSHOP_ID', 62);
 defined('SECOMEXT_EQUIPMENT_ID') || define('SECOMEXT_EQUIPMENT_ID', '1');
 defined('SECOMEXT_INSPECTOR_ID') || define('SECOMEXT_INSPECTOR_ID', '1');
