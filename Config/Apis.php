54bd36cd6bfe86742ae021248af524332ba484c0
ArzateOscar33 <oscar.arzate201@tectijuana.edu.mx>
2026-04-06 09:30:33 -0700

    Subida de Assets y Vendor

--- Config/Apis.php
+++ Config/Apis.php
@@ -12,7 +12,7 @@ defined('SECOMEXT_LOCATION_ID')  || define('SECOMEXT_LOCATION_ID', 1);
 // ===== Entorno =====
 defined('APP_ENV')  || define('APP_ENV',            'testing'); //testing o production
 defined('TEST_DATOS_RESULT')  || define('TEST_DATOS_RESULT',  'success'); //success o failed
-defined('TEST_FOTOS_RESULT')  || define('TEST_FOTOS_RESULT',  'success');
+defined('TEST_FOTOS_RESULT')  || define('TEST_FOTOS_RESULT',  'success'); //success o failed
 
 // ===== SmogsBackups (debe ir al final porque usa return) =====
 return [
